56c52986a5c4a9b9b294f4ff0bd166c6ea88f3ce
Fernando <fh1273509@gmail.com>
2022-10-17 23:12:33 -0300

    modificando para que funciones el elemento replace

--- codigo.js
+++ codigo.js
@@ -1,7 +1,18 @@
 
-id('cam').innerHTML = "";
-
 function modificar(){
     var cambiar = document.getElementById("titulo");
     cambiar.innerHTML = 'TITULO MODIFICADO';
 }
+function nuevo(){
+    var cambiar = document.getElementById("titulo");
+    cambiar.innerHTML = "TITULO A MODIFICAR";
+}
+
+
+function reemplazar(){
+    var elemento = document.getElementById('titulo');
+    contenido = document.createTextNode('TEXTO NUEVO');
+    
+    reemp.replaceChild(elemento, contenido);
+}
+
