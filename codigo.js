3a4f46918b9fa855ccf6b7e81a63ddc3a5d4e6e5
Fernando <fh1273509@gmail.com>
2022-10-18 22:50:15 -0300

    resolviendo el replace.Child

--- codigo.js
+++ codigo.js
@@ -1,18 +1,25 @@
+function modificar() {
+  var cambiar = document.getElementById("titulo");
+  cambiar.innerHTML = "MODIFICADO";
+}
+function nuevo() {
+  var cambiar = document.getElementById("titulo");
+  cambiar.innerHTML = "TITULO A MODIFICAR";
+}
 
-function modificar(){
-    var cambiar = document.getElementById("titulo");
-    cambiar.innerHTML = 'TITULO MODIFICADO';
-}
-function nuevo(){
-    var cambiar = document.getElementById("titulo");
-    cambiar.innerHTML = "TITULO A MODIFICAR";
-}
+
 
 
 function reemplazar(){
-    var elemento = document.getElementById('titulo');
-    contenido = document.createTextNode('TEXTO NUEVO');
-    
-    reemp.replaceChild(elemento, contenido);
-}
+    var nuevoH2 = document.createElement("h2");
+    var nuevoH2Texto = document.createTextNode("Bienvenido");
+    nuevoH2.appendChild(nuevoH2Texto);
 
+    var mydiv = document.getElementById("id1");
+    var viejoP = document.getElementById("para1");
+
+    mydiv.replaceChild(nuevoH2, viejoP);
+}
+window.onload = function(){
+    document.getElementById("btn").onclick = reemplazar;
+}
